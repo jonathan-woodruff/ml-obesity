2ca10977b50f3b7788692bbb567ae7e237cce851
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2025-07-30 04:07:24 -0500

    converted age feature to int

--- ml-obesity/app.py
+++ ml-obesity/app.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import numpy as np
 
 load_dotenv()  #load environment variables from .env
 
@@ -14,9 +15,20 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 df = pd.read_csv('obesity-data.csv')
 
 ### STEP 2: DATA CLEANING
+
+#Get familiar with the data
+print(df.head())
+print(df.info())
+
+print(df.columns[df.isnull().sum() > 0]) #check for null values
+
+#take the floor of the age column values and convert the age variable to int
+take_floor = lambda x: np.floor(x)
+df['Age'] = df['Age'].apply(take_floor).astype(int)
+
+### STEP 3: DATA SPLIT
 X = df.drop(columns=['NObeyesdad'])
 y = df['NObeyesdad']
-print(X.columns[X.isnull().sum()>0])
 
 @app.route('/', methods=["GET", "POST"])
 def index():
