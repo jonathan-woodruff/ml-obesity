4aa273e96035b00078fbebdfde73a57103dbb1b6
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2025-08-06 12:43:53 -0500

    normalized the numeric features

--- ml-obesity/app.py
+++ ml-obesity/app.py
@@ -19,13 +19,11 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 df = pd.read_csv('obesity-data.csv')
 
 ### STEP 2: DATA CLEANING
-
 #Get familiar with the data
 print(df.head())
 print(df.info())
 
 ### STEP 3: EXPLORATORY DATA ANALYSIS
-
 #Provide an order to the ordinal variables
 df['CAEC'] = pd.Categorical(df['CAEC'], ['no', 'Sometimes', 'Frequently', 'Always'], ordered=True)
 df['CALC'] = pd.Categorical(df['CALC'], ['no', 'Sometimes', 'Frequently', 'Always'], ordered=True)
@@ -36,22 +34,25 @@ df['CALC'] = pd.Categorical(df['CALC'], ['no', 'Sometimes', 'Frequently', 'Alway
 #plt.close()
 
 #show value counts for categorical data
-print(df['Gender'].value_counts())
-print(df['family_history_with_overweight'].value_counts())
-print(df['FAVC'].value_counts())
-print(df['CAEC'].value_counts())
-print(df['SMOKE'].value_counts())
-print(df['SCC'].value_counts())
-print(df['CALC'].value_counts())
-print(df['MTRANS'].value_counts())
+#print(df['Gender'].value_counts())
+#print(df['family_history_with_overweight'].value_counts())
+#print(df['FAVC'].value_counts())
+#print(df['CAEC'].value_counts())
+#print(df['SMOKE'].value_counts())
+#print(df['SCC'].value_counts())
+#print(df['CALC'].value_counts())
+#print(df['MTRANS'].value_counts())
+print(df['NObeyesdad'].value_counts())
 
 ### Step 4: MODEL SELECTION
 # We'll start with linear SVC
 
 ### STEP 5: FEATURE ENGINEERING
-X_numeric = df['Age', 'Height', 'Weight']
+#Z-score normalize the numeric columns
+X_numeric = df[['Age', 'Height', 'Weight']]
 scaler = preprocessing.StandardScaler().fit(X_numeric)
 X_numeric_scaled = scaler.transform(X_numeric)
+df[['Age', 'Height', 'Weight']] = X_numeric_scaled
 
 ### STEP 6: DATA SPLIT
 X = df.drop(columns=['NObeyesdad'])
